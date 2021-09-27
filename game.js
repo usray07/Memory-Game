3180e17c284579d92166df3c2aac145120725a9c
Uttam Sundar Ray <72445750+usray07@users.noreply.github.com>
2021-09-27 23:10:14 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+alert('Remember the sequence and play pattern from beginning each time');
 var buttonColours = ['green','red','yellow','blue'];
 var gamePattern = [];
 var userClicks = [];
@@ -17,7 +18,7 @@ function nextSequence(){
 
 function animate(c){
     $('#'+c).fadeOut(100).fadeIn(100);
-    var sound = new Audio('F:\\Courses\\Web Development\\Simon Game Challenge Starting Files\\sounds\\'+c+'.mp3');
+    var sound = new Audio(c+'.mp3');
     sound.play();
 }
 function animateClick(c){
@@ -26,7 +27,7 @@ function animateClick(c){
         $('#'+c).removeClass('pressed');
 
     },100); 
-    var nsound = new Audio('F:\\Courses\\Web Development\\Simon Game Challenge Starting Files\\sounds\\'+c+'.mp3');
+    var nsound = new Audio(c+'.mp3');
     nsound.play();
 }
 function clicks(){
@@ -57,7 +58,7 @@ function checkAnswer(currLevel){
     }
     else{
         // console.log('wrong');
-        var wrong = new Audio('F:\\Courses\\Web Development\\Simon Game Challenge Starting Files\\sounds\\wrong.mp3');
+        var wrong = new Audio('wrong.mp3');
         wrong.play();
         $('body').addClass('game-over');
         setTimeout(function(){
